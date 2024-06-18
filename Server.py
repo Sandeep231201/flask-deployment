e60a9c5ae5b2442135db10cdb89bddca2489df8d
Sandeep Das <das.sandeep2312@gmail.com>
2024-06-18 13:52:30 +0530

    Deployment

---
+++ Server.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+# MongoDB Setup
+url = "mongodb+srv://sandeep231201:sandeep231201@loginpagecluster.p8fykfu.mongodb.net/?retryWrites=true&w=majority&appName=loginPageCluster"
+
+client = MongoClient(url, server_api=ServerApi("1"))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# WSGI Application
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def homePage():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def registrationPage():
+    if request.method == "POST":
+        user = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        client.loginPageCluster.customer.insert_one(
+            {"username": user, "email": email, "password": password}
+        )
+        print(f"Registered user: {user}, email: {email}")
+        return redirect(url_for("loginPage"))
+
+    return render_template("registration.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def loginPage():
+    if request.method == "POST":
+        user = request.form["username"]
+        password = request.form["password"]
+        print(f"Login attempt for user: {user}")
+        result = client.loginPageCluster.customer.find_one(
+            {"username": user, "password": password}
+        )
+        if result:
+            return redirect(url_for("credentials", username=user))
+        else:
+            return "Invalid Username or Password", 401
+
+    return render_template("login.html")
+
+
+@app.route("/credentials/<username>", methods=["GET"])
+def credentials(username):
+    print(f"Fetching credentials for user: {username}")
+    user_info = client.loginPageCluster.customer.find_one({"username": username})
+    if user_info:
+        print(f"User info: {user_info}")
+        return render_template(
+            "credentials.html",
+            username=user_info["username"],
+            email=user_info["email"],
+            password=user_info["password"],
+        )
+    else:
+        print("User not found")
+        return "User not found", 404
+
+
+
